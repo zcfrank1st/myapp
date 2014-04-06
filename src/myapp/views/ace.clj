51e806361635171498246699e3aea62c6f5319c4
zcfrank1st <zhchaos@gmail.com>
2014-04-06 23:32:27 +0800

    增加信息输入删除修改 modal
    如标题

--- src/myapp/views/ace.clj
+++ src/myapp/views/ace.clj
@@ -72,7 +72,7 @@
          [:table
           [:thead
            [:tr
-            [:th ""]
+            [:th "编号"]
             [:th "姓名"]
             [:th "年龄"]
             [:th "性别"]
@@ -84,10 +84,72 @@
           [:tbody
            ]]]]
        [:div.row
-        [:div.col-xs-1
-         [:button.btn.btn-info.btn-xs {:id "add"}
+        [:div.col-xs-12
+         [:a.btn.btn-info.btn-xs
+          {:id "add" :href "#modal-form-add" :role "button" :data-toggle "modal"}
           [:i.icon-asterisk]
-          "增加"]]
-        [:div.col-xs-1
-         [:button.btn.btn-info.btn-xs {:id "edit"}
-          [:i.icon-pencil]"修改"]]]]]]))
+          "增加"]
+         "&nbsp;&nbsp;"
+         [:a.btn.btn-info.btn-xs
+          {:id "edit" :href "#modal-form-edit" :role "button" :data-toggle "modal"}
+          [:i.icon-pencil]"修改"]
+         "&nbsp;&nbsp;"
+         [:a.btn.btn-danger.btn-xs
+          {:id "del" :href "#modal-form-del" :role "button" :data-toggle "modal"}
+          [:i.icon-trash]"删除"]]]
+
+       [:div.modal.fade {:id "modal-form-add" :tabindex "-1"}
+        [:div.modal-dialog
+         [:div.modal-content
+          [:div.modal-header.no-padding
+           [:div.table-header
+            [:button.close
+             {:type "button" :data-dismiss "modal" :aria-hidden "true"}
+             [:span.white "&times;"]]
+            " 面试记录增加"]]
+          [:div.modal-body.no-padding
+           ]
+          [:div.modal-footer.no-margin-top
+           [:button.btn.btn-xs.btn-info.pull-left {:id "add-ok"}
+            [:i.icon-ok]
+            "确认"]
+           [:button.btn.btn-xs.btn-info.pull-left {:id "add-remove" :data-dismiss "modal"}
+            [:i.icon-remove]
+            "取消"]]]]]
+
+       [:div.modal.fade {:id "modal-form-edit" :tabindex "-1"}
+        [:div.modal-dialog
+         [:div.modal-content
+          [:div.modal-header.no-padding
+           [:div.table-header
+            [:button.close
+             {:type "button" :data-dismiss "modal" :aria-hidden "true"}
+             [:span.white "&times;"]]
+            " 面试记录修改"]]
+          [:div.modal-body.no-padding
+           ]
+          [:div.modal-footer.no-margin-top
+           [:button.btn.btn-xs.btn-info.pull-left {:id "edit-ok"}
+            [:i.icon-ok]
+            "确认"]
+           [:button.btn.btn-xs.btn-info.pull-left {:id "edit-remove" :data-dismiss "modal"}
+            [:i.icon-remove]
+            "取消"]]]]]
+
+       [:div.modal.fade {:id "modal-form-del" :tabindex "-1"}
+        [:div.modal-dialog
+         [:div.modal-content
+          [:div.modal-header.no-padding
+           [:div.table-header
+            [:button.close
+             {:type "button" :data-dismiss "modal" :aria-hidden "true"}
+             [:span.white "&times;"]]
+            " 是否删除选中项？"]]
+
+          [:div.modal-footer.no-margin-top
+           [:button.btn.btn-xs.btn-info.pull-left {:id "del-ok"}
+            [:i.icon-ok]
+            "确认"]
+           [:button.btn.btn-xs.btn-info.pull-left {:id "del-remove" :data-dismiss "modal"}
+            [:i.icon-remove]
+            "取消"]]]]]]]]))
